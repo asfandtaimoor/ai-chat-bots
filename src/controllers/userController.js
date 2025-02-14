fe0a37c69e75a702176ade76f686594268189dde
asfandtaimoor <asfandtaimoor786@gmail.com>
2025-02-14 14:44:51 +0500

    DONE : User controller added

---
+++ src/controllers/userController.js
@@ -0,0 +1,93 @@
+import db from '../config/db.js';
+import bcrypt from 'bcrypt';
+// Get All Users
+export const getAllUser = async (req, res) => {
+  try {
+    const [results] = await db.promise().query('SELECT * FROM Users');
+
+    if (results.length === 0) {
+      return res.status(404).json({ message: 'There are no Users' });
+    }
+
+    res.json(results);
+  } catch (err) {
+    console.error('Error fetching users:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+// Register User
+export const registerUser = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    // Check if user already exists
+    const [existingUser] = await db
+      .promise()
+      .query('SELECT * FROM Users WHERE email = ?', [email]);
+
+    if (existingUser.length > 0) {
+      return res.status(409).json({ message: 'User already exists' });
+    }
+
+    const [result] = await db
+      .promise()
+      .query(
+        'INSERT INTO Users (firstName, lastName, email, password) VALUES (?, ?, ?, ?)',
+        [firstName, lastName, email, hashedPassword]
+      );
+
+    res
+      .status(201)
+      .json({ message: 'User created successfully', userId: result.insertId });
+  } catch (err) {
+    console.error('Error creating user:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+// update User
+export const updateUser = async (req, res) => {
+  const userId = req.params.id;
+  const { firstName, lastName, password } = req.body;
+
+  try {
+    const [results] = await db
+      .promise()
+      .query(
+        'UPDATE Users SET firstName = ?, lastName = ?,   password = ? WHERE id = ?',
+        [firstName, lastName, password, userId]
+      );
+
+    console.log(results);
+    if (results.affectedRows === 0) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json({ message: 'User updated successfully' });
+  } catch (err) {
+    console.error('Error updating user:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+// Delete User
+export const deleteUser = async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const [results] = await db
+      .promise()
+      .query('DELETE FROM Users WHERE id = ?', [userId]);
+
+    if (results.affectedRows === 0) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json({ message: 'User deleted successfully' });
+  } catch (err) {
+    console.error('Error deleting user:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,98 +1,26 @@
 import express from 'express';
-import db from '../config/db.js';
-import bcrypt from 'bcrypt';
 const router = express.Router();
+import {
+  getAllUser,
+  registerUser,
+  updateUser,
+  deleteUser,
+} from '../controllers/userController.js';
 
 // ✅ Get All Users
-router.get('/', async (req, res) => {
-  try {
-    const [results] = await db.promise().query('SELECT * FROM Users');
-
-    if (results.length === 0) {
-      return res.status(404).json({ message: 'There are no Users' });
-    }
-
-    res.json(results);
-  } catch (err) {
-    console.error('Error fetching users:', err);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+router.get('/', getAllUser);
 
 // ✅ Create User
-router.post('', async (req, res) => {
-  try {
-    const { firstName, lastName, email, password } = req.body;
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-    // Check if user already exists
-    const [existingUser] = await db
-      .promise()
-      .query('SELECT * FROM Users WHERE email = ?', [email]);
-
-    if (existingUser.length > 0) {
-      return res.status(409).json({ message: 'User already exists' });
-    }
-
-    const [result] = await db
-      .promise()
-      .query(
-        'INSERT INTO Users (firstName, lastName, email, password) VALUES (?, ?, ?, ?)',
-        [firstName, lastName, email, hashedPassword]
-      );
-
-    res
-      .status(201)
-      .json({ message: 'User created successfully', userId: result.insertId });
-  } catch (err) {
-    console.error('Error creating user:', err);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+router.post('/register', registerUser);
 
 // ✅ Update User
-router.put('/:id', async (req, res) => {
-  const userId = req.params.id;
-  const { firstName, lastName, password } = req.body;
-
-  try {
-    const [results] = await db
-      .promise()
-      .query(
-        'UPDATE Users SET firstName = ?, lastName = ?,   password = ? WHERE id = ?',
-        [firstName, lastName, password, userId]
-      );
-
-    console.log(results);
-    if (results.affectedRows === 0) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    res.json({ message: 'User updated successfully' });
-  } catch (err) {
-    console.error('Error updating user:', err);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+router.put('/:id', updateUser);
 
 // ✅ Delete a user by ID
-router.delete('/:id', async (req, res) => {
-  const userId = req.params.id;
+router.delete('/:id', deleteUser);
 
-  try {
-    const [results] = await db
-      .promise()
-      .query('DELETE FROM Users WHERE id = ?', [userId]);
+// Login and Register
 
-    if (results.affectedRows === 0) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    res.json({ message: 'User deleted successfully' });
-  } catch (err) {
-    console.error('Error deleting user:', err);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+// router.post('/login', loginUser);
 
 export default router;
